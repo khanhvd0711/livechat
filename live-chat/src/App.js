dab027c339a37c05b3a37953179b86146a3ac9cd
Khanhvd <eros.yun711@gmail.com>
2018-10-02 17:24:52 +0700

    demo live chat

--- live-chat/src/App.js
+++ live-chat/src/App.js
@@ -24,16 +24,27 @@ class App extends Component {
     render() {
         return (
             <div>
-                <Header title="Demo Firebase"/>
-                <div className="columns">
-                    <div className="column is-3"></div>
-                    <div className="column is-6">
-                        <MessageList db={firebase} />
+                <div className="nChat">
+                    <div className="chat_top">
+                        <div className="supportInfo">
+                            <div className="spImg">
+                                <img src={'https://scontent.fhan2-4.fna.fbcdn.net/v/t1.0-1/p160x160/35299905_2240657562620977_956875553422442496_n.jpg?_nc_cat=104&oh=5e02df68fdf52db8e4ef49ae3b4e895b&oe=5C24AE57'}/>
+                            </div>
+                            <div className="spInfor">
+                                <p className="spName">Vũ Duy Khánh ▾</p>
+                                <p className="spHotLine">(024) 7305 0105</p>
+                            </div>
+                            <a className="logoNhanh">
+                                <img src={'https://nhanh.vn/images/logo/nhanh_black2.png'}/>
+                            </a>
+                        </div>
                     </div>
-                </div>
-                <div className="columns">
-                    <div className="column is-3"></div>
-                    <div className="column is-6">
+                    <div className="chat_body">
+                        <div className="box_chat">
+                            <MessageList db={firebase} />
+                        </div>
+                    </div>
+                    <div className="chat_footer">
                         <MessageBox db={firebase} />
                     </div>
                 </div>
